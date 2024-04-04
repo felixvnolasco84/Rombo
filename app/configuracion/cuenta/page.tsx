b87408a71c415aab20455ad3d88e5e3408c37df7
felixvnolasco84 <felix@polygonag.com>
2024-04-04 15:32:54 -0600

    Fix indentation and spacing in Cuenta page component

--- app/configuracion/cuenta/page.tsx
+++ app/configuracion/cuenta/page.tsx
@@ -2,31 +2,31 @@ import { redirect } from "next/navigation"
 import { getServerSession } from "next-auth/next"
 
 import { Separator } from "@/components/ui/separator"
- import { AccountForm } from "@/app/configuracion/cuenta/cuenta-form"
+import { AccountForm } from "@/app/configuracion/cuenta/cuenta-form"
 import { authOptions } from "@/utils/AuthOptions"
 
 export default async function Cuenta() {
-  const session = await getServerSession(authOptions)
+  const session: any = await getServerSession(authOptions)
 
   if (!session) {
     redirect("/login?callbackUrl=/configuracion/cuenta")
   }
-  
+
   return (
-    <div className="ml-4 space-y-6">
+    <div className="space-y-6 ml-4">
       <div>
-        <h3 className="text-lg font-medium">Cuenta</h3>
-        <p className="text-sm text-muted-foreground">
+        <h3 className="font-medium text-lg">Cuenta</h3>
+        <p className="text-muted-foreground text-sm">
           Actualice la configuración de su cuenta. Establezca su idioma
           preferido y zona horaria.
         </p>
       </div>
       <Separator />
-      {/* <AccountForm
-        name={session.user?.name}
-        email={session.user?.email}
-        profilePicture={session.user?.image}
-      /> */}
+      <AccountForm
+        name={session?.user?.name}
+        email={session?.user?.email}
+        profilePicture={session?.user?.image}
+      />
     </div>
   )
 }
