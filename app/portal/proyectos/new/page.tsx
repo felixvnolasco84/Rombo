985b62062d37c69f972cc7996c49629300d36de2
felixvnolasco84 <felix@polygonag.com>
2024-04-17 23:38:36 -0600

    Actualizar componente de página de proyecto y corregir formato

--- app/portal/proyectos/new/page.tsx
+++ app/portal/proyectos/new/page.tsx
@@ -1,12 +1,11 @@
 import ProjectForm from "@/components/Forms/ProjectForm";
-import RequestForm from "@/components/Forms/RequestForm";
 
 export default function page() {
     return (
-        <div className="mx-auto py-6 max-w-6xl">
-            <h1 className="mb-4 lg:mb-3 xl:mb-6 w-11/12 lg:w-11/12 text-3xl text-center lg:text-left lg:text-4xl xl:text-6xl">Crea un nuevo proyecto</h1>
-            <p className="mb-8 lg:mb-3 xl:mb-6 text-center text-xs lg:text-left lg:text-base xl:text-lg">Por favor llena el siguiente formulario para crear un nuevo proyecto.</p>
-            <ProjectForm />
+        <div className="mx-auto max-w-6xl py-6">
+            <h1 className="mb-4 w-11/12 text-center text-3xl lg:mb-3 lg:w-11/12 lg:text-left lg:text-4xl xl:mb-6 xl:text-6xl">Crea un nuevo proyecto</h1>
+            <p className="mb-8 text-center text-xs lg:mb-3 lg:text-left lg:text-base xl:mb-6 xl:text-lg">Por favor llena el siguiente formulario para crear un nuevo proyecto.</p>
+            {/* <ProjectForm /> */}
         </div>
 
     )
