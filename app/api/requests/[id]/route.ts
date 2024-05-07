a56a7027d370668af201073c9e9d3e5d24f896d7
felixvnolasco84 <felix@polygonag.com>
2024-05-07 12:00:42 -0600

    Actualizar componente de página de solicitudes y corregir formato

--- app/api/requests/[id]/route.ts
+++ app/api/requests/[id]/route.ts
@@ -1,24 +1,23 @@
-import { NextResponse } from "next/server"
-import prisma from "@/utils/ConnectionPool"
-
+import { NextResponse } from "next/server";
+import prisma from "@/utils/ConnectionPool";
 
 //GET SINGLE REQUEST
 
 export const GET = async (id: any) => {
-    try {
-        const post = await prisma.request.findUnique({
-            where: { id: id },
-            include: {
-                project: true,
-                comments: true
-            }
-        })
+  try {
+    const post = await prisma.request.findUnique({
+      where: { id: id },
+      include: {
+        brand: true,
+        comments: true,
+      },
+    });
 
-        return new NextResponse(JSON.stringify(post))
-    } catch (err) {
-        console.log(err)
-        return new NextResponse(
-            JSON.stringify({ message: "Something went wrong!" })
-        )
-    }
-}
+    return new NextResponse(JSON.stringify(post));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" })
+    );
+  }
+};
--- app/api/requests/route.ts
+++ app/api/requests/route.ts
@@ -2,57 +2,51 @@ import { NextResponse } from "next/server";
 import { getAuthSession } from "@/utils/AuthOptions";
 import prisma from "@/utils/ConnectionPool";
 
-
 //GET ALL REQUESTS
 
 export const GET = async () => {
-    const session: any = await getAuthSession();
-    if (!session) {
-        return new NextResponse(JSON.stringify({ message: "Not Authenticated!" }))
-    }
-    try {
-        // const body = await req.json();
-        const requests = await prisma.request.findMany(
-            {
-                where: {
-                    userEmail: session.user.email
-                },
-                include: {
-                    project: true,                    
-                }
-
-            }
-        );
-        return new NextResponse(JSON.stringify(requests))
-
-    } catch (error) {
-        return new NextResponse(
-            JSON.stringify({ message: "Something went wrong!" })
-        )
-    }
-}
+  const session: any = await getAuthSession();
+  if (!session) {
+    return new NextResponse(JSON.stringify({ message: "Not Authenticated!" }));
+  }
+  try {
+    // const body = await req.json();
+    const requests = await prisma.request.findMany({
+      where: {
+        userEmail: session.user.email,
+      },
+      include: {
+        brand: true,
+      },
+    });
+    return new NextResponse(JSON.stringify(requests));
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" })
+    );
+  }
+};
 
 //CREATE REQUEST
 export const POST = async (req: any) => {
-    const session: any = await getAuthSession();
+  const session: any = await getAuthSession();
 
-    if (!session) {
-        return new NextResponse(JSON.stringify({ message: "Not Authenticated!" }))
-    }
-    try {
-        const body = await req.json();
-        const request = await prisma.request.create({
-            data: {
-                ...body,
-                userEmail: session.user.email
-            }
-        });
-        return NextResponse.json({request})
-    } catch (error) {
-        console.log(error);
-        return new NextResponse(
-            JSON.stringify({ message: "Something went wrong!" })
-        )
-    }
-}
-
+  if (!session) {
+    return new NextResponse(JSON.stringify({ message: "Not Authenticated!" }));
+  }
+  try {
+    const body = await req.json();
+    const request = await prisma.request.create({
+      data: {
+        ...body,
+        userEmail: session.user.email,
+      },
+    });
+    return NextResponse.json({ request });
+  } catch (error) {
+    console.log(error);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" })
+    );
+  }
+};
