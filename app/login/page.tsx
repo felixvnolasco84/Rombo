eaecdccad44fa593629c6d04f73404bb61d3bee5
felixvnolasco84 <felix@polygonag.com>
2024-05-23 14:35:47 -0600

    chore: Update deadline default value in Request model

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default async function page() {
   if (session) {
     redirect("/");
   }
-
+ 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex flex-col items-start gap-2">
