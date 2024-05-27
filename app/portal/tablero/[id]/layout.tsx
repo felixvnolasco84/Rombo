cd92d05bdd5914a424e558dc21a381c7ac740816
felixvnolasco84 <felix@polygonag.com>
2024-05-27 11:09:11 -0600

    Update BoardLayout component to remove commented code and unused imports

--- app/portal/tablero/[id]/layout.tsx
+++ app/portal/tablero/[id]/layout.tsx
@@ -9,16 +9,16 @@ const BoardLayout = async ({
   children: React.ReactNode;
   params: { boardId: string };
 }) => {
-  const board: any = await prisma.board.findUnique({
-    where: { id: params.boardId },
-    include: { Users: true },
-  });
+  // const board: any = await prisma.board.findUnique({
+  //   where: { id: params.boardId },
+  //   include: { Users: true },
+  // });
   return (
     <div
       className="relative h-[85vh] bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${board?.image})` }}
+      // style={{ backgroundImage: `url(${board?.image})` }}
     >
-      <BoardNavbar board={board} />
+      {/* <BoardNavbar board={board} /> */}
       {/* <div className="absolute inset-0 bg-black/10" /> */}
       <div>{children}</div>
     </div>
